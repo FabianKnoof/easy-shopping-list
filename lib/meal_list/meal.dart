732ca6b62ebffb9367178ed50bc5db9256507082
Fabian Knoof <fabian.knoof@hotmail.com>
2022-02-22 12:19:09 +0100

    Added meal class and generated hive typeadapters

---
+++ lib/meal_list/meal.dart
@@ -0,0 +1,21 @@
+import 'package:easy_shopping_list/shopping_list/article.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+part 'meal.g.dart';
+
+@HiveType(typeId: 2)
+class Meal extends HiveObject {
+  @HiveField(0)
+  String name = "";
+  @HiveField(1)
+  double quantity = 1;
+  @HiveField(2)
+  final String quantityUnit = "Portion(en)";
+  @HiveField(3)
+  List<Article> ingredients = [];
+
+  @override
+  String toString() {
+    return "$name $quantity$quantityUnit $ingredients";
+  }
+}
---
+++ lib/meal_list/meal.g.dart
@@ -0,0 +1,48 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'meal.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class MealAdapter extends TypeAdapter<Meal> {
+  @override
+  final int typeId = 2;
+
+  @override
+  Meal read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Meal()
+      ..name = fields[0] as String
+      ..quantity = fields[1] as double
+      ..ingredients = (fields[3] as List).cast<Article>();
+  }
+
+  @override
+  void write(BinaryWriter writer, Meal obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.name)
+      ..writeByte(1)
+      ..write(obj.quantity)
+      ..writeByte(2)
+      ..write(obj.quantityUnit)
+      ..writeByte(3)
+      ..write(obj.ingredients);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is MealAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
