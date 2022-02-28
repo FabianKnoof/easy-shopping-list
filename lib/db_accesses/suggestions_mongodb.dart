5a9390c09c19b16165926874c9fb4af9b1a5b532
Fabian Knoof <fabian.knoof@hotmail.com>
2022-02-28 13:52:48 +0100

    Removed old mongodb access

--- lib/db_accesses/suggestions_mongodb.dart
+++ lib/db_accesses/suggestions_mongodb.dart
@@ -5,7 +5,6 @@ import 'package:easy_shopping_list/db_accesses/suggestions_hive.dart';
 import 'package:easy_shopping_list/meal_list/meal.dart';
 import 'package:easy_shopping_list/secrets.dart';
 import 'package:easy_shopping_list/shopping_list/article.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:http/http.dart' as http;
 
 class SuggestionsMongoDB {
@@ -140,55 +139,5 @@ class SuggestionsMongoDB {
     _httpPost("insertOne", body);
     body.remove("document");
     return true;
-  }
-}
-
-class MongoDBAccess {
-  static final String _url =
-      "https://data.mongodb-api.com/app/data-jtfko/endpoint/data/beta/action";
-
-  static final Map<String, String> _headers = {
-    'Content-Type': 'application/json',
-    'Access-Control-Request-Headers': '*',
-    'api-key': apiKeyMongoDB
-    // Todo don't do this
-  };
-
-  static final Map<dynamic, dynamic> _bodyArticleSuggestions = {
-    "dataSource": "ESLDB",
-    "database": "Suggestions",
-    "collection": "ArticleSuggestions"
-  };
-
-  static Future<List<String>> getSuggestions() async {
-    Map<dynamic, dynamic> body = _bodyArticleSuggestions;
-    body["projection"] = {"_id": 0, "Suggestion": 1};
-
-    var response = await http.post(Uri.parse(_url + "/find"),
-        headers: _headers, body: jsonEncode(body));
-    var decodedResponse = jsonDecode(utf8.decode(response.bodyBytes)) as Map;
-    List documentsResponse = decodedResponse["documents"];
-    documentsResponse.removeWhere((element) => element.isEmpty);
-    List<String> suggestions =
-        documentsResponse.map((e) => e["Suggestion"] as String).toList();
-    return suggestions;
-  }
-
-  static void syncArticleSuggestions() async {
-    Map<dynamic, dynamic> body = _bodyArticleSuggestions;
-    body["projection"] = {"_id": 0, "Version": 1};
-
-    var response = await http.post(Uri.parse(_url + "/findOne"),
-        headers: _headers, body: jsonEncode(body));
-    var decodedResponse = jsonDecode(utf8.decode(response.bodyBytes)) as Map;
-    int version = decodedResponse["document"]["Version"];
-
-    if (version >
-        Hive.box("Suggestions")
-            .get("VersionArticleSuggestions", defaultValue: 0)) {
-      log("Suggestions update available");
-      Hive.box("Suggestions").put("ArticleSuggestions", await getSuggestions());
-      Hive.box("Suggestions").put("VersionArticleSuggestions", version);
-    }
   }
 }
